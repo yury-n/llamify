52387219136f1659286ef7f31bc193ab995483dc
yury-n <yury.nedomolkin@gmail.com>
2020-08-31 02:05:44 +0400

    show search input only in list view

--- pages/index.js
+++ pages/index.js
@@ -627,19 +627,21 @@ const Index = () => {
                 {showStartTeamForm && <StartTeamForm onStartTeam={startTeam} />}
                 {showTeamDirectoty && (
                   <div className="directory">
-                    <div className="input-wrapper input-search-wrapper">
-                      <img src="/icons/magnifying_glass.svg" />
-                      <input
-                        className="input input-search"
-                        id="email"
-                        type="text"
-                        autoComplete="off"
-                        spellCheck={false}
-                        placeholder="Search by name or role"
-                        value={searchString}
-                        onChange={(e) => setSearchString(e.target.value)}
-                      />
-                    </div>
+                    {viewMode === "list" && (
+                      <div className="input-wrapper input-search-wrapper">
+                        <img src="/icons/magnifying_glass.svg" />
+                        <input
+                          className="input input-search"
+                          id="email"
+                          type="text"
+                          autoComplete="off"
+                          spellCheck={false}
+                          placeholder="Search by name or role"
+                          value={searchString}
+                          onChange={(e) => setSearchString(e.target.value)}
+                        />
+                      </div>
+                    )}
                     <NewPostsToggle
                       timeframe={timeframe}
                       onSetTimeframe={onSetTimeframe}
